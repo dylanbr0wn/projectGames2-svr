a73f522de9518310edcaad41163e07fd1d7b470e
dylanbr0wn <dylan.bro97@gmail.com>
2020-04-18 14:20:48 -0700

    switch to single page lookup, add ratings

--- app.js
+++ app.js
@@ -2,44 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-// var request = require('request');
 const axios = require('axios');
-var instance = axios.create({
+
+const instance = axios.create({
     baseURL: "https://api-v3.igdb.com/",
     headers: {
-            'user-key': process.env.IGDB_KEY,
-            'Content-Type': 'text/plain'
-        },
+        'user-key': process.env.IGDB_KEY,
+        'Content-Type': 'text/plain'
+    },
 })
-// var mongoose = require('mongoose')
-//
-//
-//
-// var url = process.env.MONGOLAB_URI;
-// mongoose.connect(url);
-// var db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error"));
-// db.once("open", function(callback){
-//     console.log("Connection Succeeded");
-// });
-//
-// var Schema = mongoose.Schema;
-//
-// var GameSchema = new Schema({
-//   title: String,
-//   urlgog: String,
-//   urlsteam: String,
-//   developer: String,
-//   publisher: String,
-//   releasedate: String,
-//   discountpricegog: String,
-//   discountpricesteam: String,
-//   pricegog: String,
-//   pricesteam: String,
-//   rating: String,
-// });
-//
-// var Game = mongoose.model("Game", GameSchema);
+
 
 const app = express()
 app.use(morgan('combined'))
@@ -47,18 +19,20 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-app.get('/gameList', (req, res) => {
-  Game.find({}, function (error, games) {
-	  if (error) { console.error(error); }
-	  res.send({
-			games: games
-		})
-	}).sort({_id:-1})
+app.get('/listCover', (req, res) => {
+    instance.get('covers/', {
+        data: `fields image_id; where id = (${req.query.coverList.join(',')});`
+    }).then(function (response) {
+
+        res.send(response.data);
+    }).catch(function (error) {
+        console.log(error);
+    });
 })
 
 app.get('/games', (req, res) => {
     instance.get('games/',{
-        data: `fields name,release_dates.human,artworks,involved_companies,aggregated_rating,summary,genres; where version_parent = null & summary != null; limit ${req.query.itemsPerPage}; offset ${req.query.itemsPerPage * req.query.page};`
+        data: `fields name,release_dates.human,cover,involved_companies,aggregated_rating,summary,genres; where version_parent = null & summary != null; limit ${req.query.itemsPerPage}; offset ${req.query.itemsPerPage * (req.query.page - 1)};`
     }).then(function (response) {
         res.send(response.data);
     }).catch(function (error) {
@@ -67,109 +41,26 @@ app.get('/games', (req, res) => {
   })
 
 app.post('/game', (req, res) => {
-    var db = req.db;
-    
-    Game.findOne({ title: req.body.title }, function (error, games) {
-        
-        if (error) { console.error(error); }
-        if (games) {
-            console.log('updated game');
-            games.discountpricegog = req.body.discountpricegog;
-            games.discountpricesteam = req.body.discountpricesteam;
-            games.pricegog = req.body.pricegog;
-            games.pricesteam = req.body.pricesteam;
-            games.publisher = req.body.publisher;
-            games.developer = req.body.developer;
-            games.rating = req.body.rating;
-            games.releasedate = req.body.releasedate;
-            // console.log(games);
-            games.save(function (error) {
-                if (error) {
-                    console.log(error)
-                }
-                res.send({
-                    success: true
-                })
-            })
-        } else {
-            console.log('new game');
-            var title = req.body.title;
-            var urlgog = req.body.urlgog;
-            var urlsteam = req.body.urlsteam;
-            var developer = req.body.developer;
-            var publisher = req.body.publisher;
-            var releasedate = req.body.releasedate;
-            var pricegog = req.body.pricegog;
-            var pricesteam = req.body.pricesteam;
-            var discountpricegog = req.body.discountpricegog;
-            var discountpricesteam = req.body.discountpricesteam;
-            var rating = req.body.rating;
-            var new_Game = new Game({
-                title: title,
-                urlgog : urlgog ,
-                urlsteam : urlsteam ,
-                releasedate : releasedate,
-                publisher : publisher,
-                developer : developer,
-                pricegog: pricegog,
-                pricesteam: pricesteam,
-                discountpricegog : discountpricegog,
-                discountpricesteam : discountpricesteam,
-                rating : rating,
-
-            })
-
-            new_Game.save(function (error) {
-                if (error) {
-                    console.log(error)
-                }
-                res.send({
-                    success: true
-                })
-            })
-        }
-
-
-    })
+    res.send()
 })
 
 app.put('/game/:id', (req, res) => {
-    var db = req.db;
-    
-    Game.findOne({ id_: req.params.id }, function (error, games) {
-        
-
-
-    })
+    res.send()
 
 })
 
 app.delete('/game/:id', (req, res) => {
-	var db = req.db;
-	Game.remove({
-		_id: req.params.id
-	}, function(err, game){
-		if (err)
-			res.send(err)
-		res.send({
-			success: true
-		})
-	})
+    res.send()
 })
 
 app.get('/game/:id', (req, res) => {
-	var db = req.db;
-	Game.findById(req.params.id, function (error, game) {
-	  if (error) { console.error(error); }
-	  if (game){
-        res.send(game)
-      }
-	})
+    res.send()
 })
 
 app.get('/search', (req, res) => {
+    let query = `search "${req.query.search}"; fields name,release_dates.human,cover,involved_companies,aggregated_rating,summary,genres;limit ${req.query.itemsPerPage}; offset ${req.query.itemsPerPage * (req.query.page - 1)};`
     instance.get('games/',{
-        data: `search "${req.query.search}"; fields name,release_dates.human,artworks,involved_companies,aggregated_rating,summary,genres;`
+        data: query
     }).then(function (response) {
         res.send(response.data);
     }).catch(function (error) {
